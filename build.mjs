30d5099d58238f20fe1c8dad9296b8211473a70d
Joe Reddie <joseph.reddie@itv.com>
2024-07-02 11:27:04 +0100

    Convert to async await and build list of post previews

--- build.mjs
+++ build.mjs
@@ -1,47 +1,87 @@
 import { marked } from "marked";
-import fs from "fs";
+import fs from "fs/promises";
 import path from "path";
+import { parse } from "node-html-parser";
+
 const postsDir = "./posts";
 const buildDir = "./build";
+const filesToCopy = ["./index.css", "./index.js"];
 
-const filesToCopy = ["./index.html", "./index.css", "./index.js"];
+const getPreviewHTML = ({ filePath, firstHeading, secondHeading }) => {
+  return `
+<li>
+    <article>
+      <a href="${filePath}">
+          <header>
+              <h2>${firstHeading}</h2>
+              <p>${secondHeading}</p>
+          </header>
+          <figure>
+              <img src="path/to/image1.jpg" alt="Description of image 1">
+              <figcaption>Optional caption for the image</figcaption>
+          </figure>
+        </a>
+    </article>
+</li>`;
+};
 
-fs.rm(buildDir, { recursive: true }, (err) => {
-  if (err) {
-    console.log(err);
-  }
-  console.log("Directory deleted!");
-  fs.mkdir(buildDir, {}, (err) => {
-    if (err) {
-      return console.log(err);
-    }
+const processMarkdownFiles = async () => {
+  try {
+    // Remove the build directory if it exists
+    await fs.rm(buildDir, { recursive: true, force: true });
+    console.log("Directory deleted!");
+
+    // Create the build directory
+    await fs.mkdir(buildDir, { recursive: true });
     console.log("Directory Created!");
-    fs.readdir(postsDir, (err, files) => {
-      if (err) {
-        throw new Error(err);
-      }
-      files.forEach((file) => {
-        const filePath = path.join(postsDir, file);
-        fs.readFile(filePath, { encoding: "utf-8" }, (err, data) => {
-          const html = marked.parse(data);
-          const htmlFilePath =
-            filePath.substring(0, file.lastIndexOf(".")) + ".html";
-          fs.writeFile(`build/${htmlFilePath}`, html, (err) => {
-            if (err) {
-              return console.log(err);
-            }
-            console.log("The file was saved!");
-          });
-        });
-      });
-    });
 
-    filesToCopy.forEach((file) => {
-      fs.copyFile(file, path.join(buildDir, file), (err) => {
-        if (err) {
-          return console.log(err);
-        }
-      });
-    });
-  });
-});
+    // Read files from posts directory
+    const files = await fs.readdir(postsDir);
+
+    // Parse index.html
+    const indexHtml = await fs.readFile("./index.html", { encoding: "utf-8" });
+    const root = parse(indexHtml);
+    const list = root.getElementById("post-list");
+
+    for (const file of files) {
+      const filePath = path.join(postsDir, file);
+
+      // Read and parse markdown file
+      const postMd = await fs.readFile(filePath, { encoding: "utf-8" });
+      const html = marked.parse(postMd);
+      const htmlFilePath = path.join(buildDir, `${path.parse(file).name}.html`);
+
+      // Write HTML file to build directory
+      await fs.writeFile(htmlFilePath, html);
+      console.log("The file was saved!");
+
+      const tokens = marked.lexer(postMd);
+      const firstHeading =
+        tokens.find((token) => token.type === "heading" && token.depth === 1)
+          ?.text || "";
+      const secondHeading =
+        tokens.find((token) => token.type === "heading" && token.depth === 2)
+          ?.text || "";
+
+      // Append preview HTML to list
+      list.appendChild(
+        parse(getPreviewHTML({ firstHeading, secondHeading, filePath }))
+      );
+    }
+
+    // Write updated index.html to build directory
+    await fs.writeFile(path.join(buildDir, "index.html"), root.toString());
+    console.log("The index.html was saved!");
+
+    // Copy additional files
+    for (const file of filesToCopy) {
+      await fs.copyFile(file, path.join(buildDir, path.basename(file)));
+      console.log(file + " copied");
+    }
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+// Execute the script
+processMarkdownFiles();
